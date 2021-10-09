83b7fe8a3f55b6e15abe8a8c0755b0e88a0d2a88
Sivan Kaplan <sivanagar@gmail.com>
2021-10-09 16:41:13 -0700

    server and html route

---
+++ server.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const { db } =require('./db/db');
+const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`API Server now on port ${PORT}`);
+})
+
