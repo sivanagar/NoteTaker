34422ab1d1891242b5e3520952c09bb58ebcaa7f
Sivan Kaplan <sivanagar@gmail.com>
2021-10-09 17:24:39 -0700

    api route get and push

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,17 +1,25 @@
 const router = require('express').Router();
-
-router.get('/api/notes', (req,res) => {
-
-    //should read the db.json file and return all saved notes as JSON.
+const  {createNewNote} = require('../../lib/notes');
+const  db  = require('../../db/db');
 
 
+
+router.get('/notes', (req,res) => {
+    let results = db
+    res.json(results);
 })
 
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
+        req.body.id = db.length.toString();
+        const note = createNewNote(req.body, db)
+        res.json(db);
+    })  
+
+
     //should receive a new note to save on the request body, 
     // add it to the db.json file, and then return the new note to the client. 
     // You'll need to find a way to give each note a unique id when it's saved 
     // (look into npm packages that could do this for you).
-})
+
 
 module.exports = router;
