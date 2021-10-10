064c3d150a5d30c586afeb0421c3d5388e2c0df2
Sivan Kaplan <sivanagar@gmail.com>
2021-10-09 18:46:43 -0700

    delete note api

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const  {createNewNote} = require('../../lib/notes');
+const  {createNewNote , deleteNote} = require('../../lib/notes');
 const  db  = require('../../db/db');
 
 
@@ -15,11 +15,14 @@ router.post('/notes', (req, res) => {
         res.json(db);
     })  
 
-
-    //should receive a new note to save on the request body, 
-    // add it to the db.json file, and then return the new note to the client. 
-    // You'll need to find a way to give each note a unique id when it's saved 
-    // (look into npm packages that could do this for you).
+router.delete('/notes/:id', (req, res) => {
+    const id = req.params.id.toString()
+    if(deleteNote(id,db)) {
+        res.status(200).send('The note deleted!');
+    } else {
+    //should send the note to deleteNote function
+    res.status(400).send('There is not note to delete');
+}})
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const express = require('express');
 const { db } =require('./db/db');
 const PORT = process.env.PORT || 3001;
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes/index.js');
+const htmlRoutes = require('./routes/htmlRoutes/index');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +12,9 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
+
+
+
 
 
 app.listen(PORT, () => {
