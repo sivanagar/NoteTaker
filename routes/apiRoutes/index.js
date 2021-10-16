c0b0543be7f423e0518b21ff6af4d4c1eaa5f4cb
Sivan Kaplan <sivanagar@gmail.com>
2021-10-16 10:21:28 -0700

    fixed delete bug

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,28 +1,56 @@
 const fs = require('fs');
 const path = require("path");
 const router = require('express').Router();
-const  {createNewNote , deleteNote} = require('../../lib/notes');
+//const  {createNewNote , deleteNote} = require('../../lib/notes');
 const  db  = require('../../db/db');
 const { v4: uuidv4 } = require('uuid');
+let notes = db;
+
+
+const  writeFile = (array) => {
+    //update file function
+    fs.writeFileSync(path.join(__dirname, '../../db/db.json'), JSON.stringify(array,null, 2))
+}
+
+function deleteNote(id) {
+    //check if note exists
+    if (notes.filter(note => note.id === id).length===0) { 
+        return false;
+    } else {
+        //update notes and file
+        notes = notes.filter(note => note.id !== id);
+        writeFile(notes)
+        return true;
+ }
+}   
+
+function createNewNote(body) {
+    const note = body;
+    //update notes array and file
+    notes.push(note);
+    writeFile(notes)
+    return note;
+}
+
 
 router.get('/notes', (req,res) => {
-    res.json(db);
+    res.json(notes);
 })
 
 router.post('/notes', (req, res) => {
         req.body.id = uuidv4();
         const note = createNewNote(req.body)
-        res.json(db);
+        res.json(note);
 })  
 
 router.delete('/notes/:id', (req, res) => {
     const id = req.params.id.toString()
     if(deleteNote(id)) {
-        res.status(200).send('The note deleted!');
+        res.json(id);
     } else {
-    
     res.status(400).send('There is not note to delete');
-}})
+    }
+})
 
 
 module.exports = router;
